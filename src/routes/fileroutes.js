90a0ab8c4326aaa8573d02b16d4c450afc50df48
James Shamala <shamalajames@gmail.com>
2022-04-10 17:08:52 +0530

    files added

--- src/routes/fileroutes.js
+++ src/routes/fileroutes.js
@@ -7,15 +7,29 @@ var app = express();
 app.set('view engine', 'ejs');
 const utf8 = require('utf8');
 
+
 app.post('/register', function (req, res) {
     let query = `INSERT INTO user (name,password,email) VALUES (?, ?,?);`;
     mysqlConnection.query(query, [req.body.lname + req.body.fname, req.body.password, req.body.email], (err, rows) => {
         if (err) throw err;
         res.json({ message: "Registration has been completed successfully" });
     });
-
-
 });
+
+app.get('/getUserName/:id', function (req, res) {
+    const { id } = req.params;
+    let query = `select name from user where id=?;`;
+    mysqlConnection.query(query, [id], (err, rows) => {
+        if (err) throw err;
+        if (rows.length > 0) {
+            res.json({userid:rows[0].name });
+        }
+        else {
+            res.status(404).json({message:"username  not found"}).end() ;
+        }
+    });
+});
+
 app.post('/login', function (req, res) {
 
     let query = `select *  from user where email=? and password=?;`;
@@ -28,12 +42,9 @@ app.post('/login', function (req, res) {
             res.status(301).json({message:"Username/Password incorrect"}).end() ;
         }
     });
-
-
 });
 app.get('/getall', function (req, res) {
     mysqlConnection.query("select * from document", (err, rows, fields) => {
-        console.log(rows);
         if (!err) {
             res.json(rows).end();
         }
@@ -48,21 +59,19 @@ app.get('/', function (req, res) {
 });
 app.post("/upload", upload.array("file"), uploadFiles);
 function uploadFiles(req, res) {
-
     let ext = req.files[0].originalname.split(".")[req.files[0].originalname.split(".").length - 1];
     let query = `INSERT INTO document  (path, extension,originalname,user_id) VALUES (?, ?, ?,?);`;
     mysqlConnection.query(query, [req.files[0].path,
-        ext, req.files[0].originalname.replace('\\', '/'), 1], (err, rows) => {
+        ext, req.files[0].originalname.replace('\\', '/'), req.body.userid], (err, rows) => {
             if (err) throw err;
             res.json({ message: "File has been uploaded successfully " });
         });
 
 }
-app.get('/download/:filename/', function (req, res) {
+app.get('/download', function (req, res) {
 
-    const filename = req.params.filename;
-    console.log(filename);
-    //const file = `${__dirname}/upload-folder/dramaticpenguin.MOV`;
-    // res.download(file); // Set disposition and send it.
+    const filename = req.body.fileName;
+    console.log(req.body);
+   
 });
 module.exports = app;
--- src/script.js
+++ src/script.js
@@ -22,9 +22,9 @@ async function loadData() {
 }
 */
 
-$(document).ready(function(){
+$(document).ready(function () {
     $("#file-upload").hide();
-  });
+});
 function createRow(myObj) {
     const tableBody = document.getElementById("tdata");
     var tr = document.createElement('TR');
@@ -51,7 +51,7 @@ function createRow(myObj) {
         var linkText = document.createTextNode("Download");
         a.appendChild(linkText);
         a.title = "Download";
-        a.href = "/file/download/"+myObj.path.split("\\")[1];
+        a.href = "/file/download/" + myObj.path.split("\\")[1];
         td.appendChild(a);
         tr.appendChild(td);
     }
@@ -65,16 +65,33 @@ function createRow(myObj) {
 
 
 }
-function download(obj) {
-    console.log(obj);
+function btn_click(val){
+    var url = "/filemanagement/download/";
+ 
+    $.ajax({
+        type: "GET",
+        url: url,
+        data:{"filename":val},
+        success: function (data) {
+
+        },
+        failure: function (errMsg) {
+            alert(errMsg);
+        }
+    });
+    return ;
 }
 async function uploadFile() {
+
     let formData = new FormData();
     formData.append("file", fileupload.files[0]);
+    formData.append("userid",document.getElementById('userid').value);
+    console.log(formData);
     await fetch('/filemanagement/upload', {
         method: "POST",
         body: formData
     });
+    callFileManagement();
 }
 
 function formDataToJSON(form) {
@@ -82,83 +99,114 @@ function formDataToJSON(form) {
     let formData = form.serialize();
     let formArray = formData.split("&");
 
-    for (inputData of formArray){
+    for (inputData of formArray) {
         let dataTmp = inputData.split('=');
         obj[dataTmp[0]] = dataTmp[1];
     }
     return JSON.stringify(obj);
 }
 
-$("#submitButtonId").click(function(event) {
-    var url = "/filemanagement/register"; 
-    var formData=formDataToJSON( $('#regForm'));
+$("#submitButtonId").click(function (event) {
+    var url = "/filemanagement/register";
+    var formData = formDataToJSON($('#regForm'));
     const obj = JSON.parse(formData);
     $.ajax({
-           type: "POST",
-           url: url,
-           contentType: "application/json",
-           data: JSON.stringify( obj ),
-           success: function(data)
-           {
-               alert(data.message);
-               $("#regForm")[0].reset();
-           },
-           failure: function(errMsg) {
+        type: "POST",
+        url: url,
+        contentType: "application/json",
+        data: JSON.stringify(obj),
+        success: function (data) {
+            alert(data.message);
+            $("#regForm")[0].reset();
+        },
+        failure: function (errMsg) {
             alert(errMsg);
-          }
-         });
+        }
+    });
 
-    return false; 
+    return false;
 });
-function callFileManagement(){
+
+function getUserName(userid){
+    
+    var url = "/filemanagement/getUserName/"+userid;
+    let val='';
+    $.ajax({
+        async: false,
+        type: "GET",
+        url: url,
+        success: function (data) {
+            val=data.userid;
+        },
+        failure: function (errMsg) {
+            alert(errMsg);
+        }
+    });
+    return val;
+}
+
+function callFileManagement() {
     $("#file-upload").show();
-    var url = "/filemanagement/getAll"; 
-    var formData=formDataToJSON( $('#regForm'));
+    var url = "/filemanagement/getAll";
+    var formData = formDataToJSON($('#regForm'));
     const obj = JSON.parse(formData);
     $.ajax({
-           type: "GET",
-           url: url,
-           success: function(data)
-           {
+        type: "GET",
+        url: url,
+        success: function (data) {
             console.log(data);
-               $("#tdata").children("tr").remove();
-               for (let i = 0; i < data.length; i++) {
-                
-                createRow(response[i]);
-                }
-               
-           },
-           failure: function(errMsg) {
+            $("#tdata").children("tr").remove();
+            var j=1;
+            if(data.length>0){
+                for (let i = 0; i < data.length; i++) {
+                    console.log(  data[i].user_id);
+                      $("#tdata").append(
+                          "<tr>"
+                        +  "<td>"+ j++ +"</td>"
+                        +  "<td>"+ data[i].originalname +"</td>"
+                        +  "<td>"+ getUserName(data[i].user_id) +"</td>"
+                        +  "<td> <button type='button' value='"+ data[i].path+"."+data[i].extension+"' onclick='btn_click(this.value)'>Download</button</td></tr>"
+                      );
+                  }
+            }
+            else{
+                $("#tdata").append(
+                    "<tr>"
+                  +  "<td colspan='4'>No Record Available</td>"
+                 
+                  +  "</tr>"
+                );
+            }
+            
+        },
+        failure: function (errMsg) {
             alert(errMsg);
-          }
-         });
+        }
+    });
 
-    return false; 
-
+    return false;
 }
-$("#login-button").click(function(event) {
-    var url = "/filemanagement/login"; 
-    var formData=formDataToJSON( $('#login-form'));
+$("#login-button").click(function (event) {
+    var url = "/filemanagement/login";
+    var formData = formDataToJSON($('#login-form'));
     const obj = JSON.parse(formData);
     console.log(obj);
     $.ajax({
-           type: "POST",
-           url: url,
-           contentType: "application/json",
-           data: JSON.stringify( obj ),
-           success: function(data)
-           {
-               console.log(data.message);
-               $("#login-form")[0].reset();
-               $("#login-register").hide();
-              
-               $('#userid').val(data.userid);
-               callFileManagement();
-           },
-           error: function(data) {
-                  alert(data.responseJSON.message);
-          }
-         });
+        type: "POST",
+        url: url,
+        contentType: "application/json",
+        data: JSON.stringify(obj),
+        success: function (data) {
+            console.log(data.message);
+            $("#login-form")[0].reset();
+            $("#login-register").hide();
+            $('#userid').val(data.userid);
+            callFileManagement();
+        },
+        error: function (data) {
+            alert(data.responseJSON.message);
+        }
+    });
 
     return false;
 });
